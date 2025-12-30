646f0095eed1448d29506cccadf1aeb94bc03701
Mohd Saif <mohdfaiz.faiz2008@gmail.com>
2025-12-30 15:47:38 +0530

    deploy

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import http from "http"
+import { Server } from "socket.io";
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", // React app
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("send_message", (data) => {
+    // send message to all users
+    io.emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
