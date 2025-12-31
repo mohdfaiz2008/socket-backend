e412c63616530096d2ba0a69da4a88f390dc02a0
Mohammad Faiz <mohdfaiz.faiz2008@gmail.com>
2025-12-31 15:12:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // React app
+    origin: "https://elegant-rabanadas-0c35c4.netlify.app/", // React app
     methods: ["GET", "POST"],
   },
 });
